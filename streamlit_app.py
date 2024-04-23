ec9aa8e8b4f9c092f8892588132a96f9a95a0384
SourabhYejare <67822146+SourabhYejare@users.noreply.github.com>
2024-04-23 14:43:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ session=cnx.session()
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
 st.write(""" 
-Choose the fruits you want in your custome Smoothie"""
+Choose the fruits you want in your custom Smoothie"""
 )
 
 
